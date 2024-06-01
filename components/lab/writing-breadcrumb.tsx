ba036061e3421a5faa07be2b3d564cfaa74a4720
JingHuangSu1996 <jing.tech.tw@gmail.com>
2024-06-01 22:42:21 +0800

    chore: improvement

--- components/lab/writing-breadcrumb.tsx
+++ components/lab/writing-breadcrumb.tsx
@@ -8,6 +8,7 @@ import {
 } from '@/components/lab/ui/breadcrumb';
 import { upperFirst } from '@/lib/utils';
 import { Home } from 'lucide-react';
+import { Fragment } from 'react';
 
 type breadcrumbType = {
   name: string;
@@ -30,12 +31,12 @@ export const WritingBreadcrumb = ({ path, className }: { path: string; className
         {parsedUrl.map(({ name, url }, idx) => {
           if (!name || !url || idx === parsedUrl.length - 1) return null;
           return (
-            <>
-              <BreadcrumbItem key={url}>
+            <Fragment key={url}>
+              <BreadcrumbItem>
                 <BreadcrumbLink href={url}>{name === 'writing' ? <Home size={16} /> : upperFirst(name)}</BreadcrumbLink>
               </BreadcrumbItem>
               {idx !== parsedUrl.length - 2 && <BreadcrumbSeparator key={idx} />}
-            </>
+            </Fragment>
           );
         })}
       </BreadcrumbList>
